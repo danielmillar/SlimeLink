4ca8efc8c99847a7a1af950c21e38cb08bf6d2e2
Daniel Millar <daniel@danielmillar.dev>
2025-04-22 11:54:02 +0100

    project setup

---
+++ src/main/kotlin/dev/danielmillar/slimeLink/SlimeLink.kt
@@ -0,0 +1,14 @@
+package dev.danielmillar.slimeLink
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SlimeLink : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
