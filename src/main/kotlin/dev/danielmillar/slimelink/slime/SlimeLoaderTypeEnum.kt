44988f464c5ee3060fb07418416fe725e01eec73
Daniel Millar <daniel@danielmillar.dev>
2025-05-20 19:50:00 +0100

    Add EffCloneWorld

--- src/main/kotlin/dev/danielmillar/slimelink/slime/SlimeLoaderTypeEnum.kt
+++ src/main/kotlin/dev/danielmillar/slimelink/slime/SlimeLoaderTypeEnum.kt
@@ -3,5 +3,11 @@ package dev.danielmillar.slimelink.slime
 enum class SlimeLoaderTypeEnum(val id: String) {
     FILE("file"),
     MYSQL("mysql"),
-    MONGODB("mongodb")
+    MONGODB("mongodb");
+
+    companion object {
+        fun fromId(id: String): SlimeLoaderTypeEnum? {
+            return entries.find { it.id.equals(id, ignoreCase = true) }
+        }
+    }
 }
