974feaec99e550177d63b8d56965adbe19795b2e
Daniel Millar <daniel@danielmillar.dev>
2026-01-09 17:57:53 +0000

    Update version to 2.0.0-beta.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,55 +1,64 @@
 plugins {
-    kotlin("jvm") version "2.3.0-RC3"
+    kotlin("jvm") version "2.3.0"
     id("com.gradleup.shadow") version "9.3.0"
 }
 
 group = "dev.danielmillar"
-version = "1.1.2"
+version = "2.0.0-beta.1"
 
 repositories {
+    mavenLocal()
     mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/") {
-        name = "papermc-repo"
-    }
-    maven("https://oss.sonatype.org/content/groups/public/") {
-        name = "sonatype"
-    }
-    maven("https://repo.skriptlang.org/releases") {
-        name = "skriptlang-repo"
-    }
-    maven("https://repo.infernalsuite.com/repository/maven-snapshots/"){
-        name = "infernal-repo"
-    }
+    maven("https://repo.papermc.io/repository/maven-public/") { name = "papermc-repo" }
+    maven("https://oss.sonatype.org/content/groups/public/") { name = "sonatype" }
+    maven("https://repo.skriptlang.org/releases") { name = "skriptlang-repo" }
+    maven("https://repo.infernalsuite.com/repository/maven-snapshots/"){ name = "infernal-repo" }
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+
     compileOnly("com.github.SkriptLang:Skript:2.13.2")
+
     compileOnly("com.infernalsuite.asp:api:4.0.0-SNAPSHOT")
+    implementation("com.infernalsuite.asp:loaders:4.0.0-SNAPSHOT")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.infernalsuite.asp:loaders:4.0.0-SNAPSHOT")
-    implementation("org.spongepowered:configurate-yaml:4.2.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+
+    implementation("org.spongepowered:configurate-yaml:4.2.0-GeyserMC-SNAPSHOT")
+    implementation("org.spongepowered:configurate-extra-kotlin:4.2.0-GeyserMC-SNAPSHOT")
 }
 
 val targetJavaVersion = 25
 kotlin {
     jvmToolchain(targetJavaVersion)
+    compilerOptions {
+        javaParameters = true // Handles parameter names for both Kotlin and Java
+        freeCompilerArgs.set(listOf("-Xannotation-default-target=param-property"))
+    }
 }
 
-tasks.build {
-    dependsOn("shadowJar")
-}
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
 
-tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
-    archiveClassifier.set("")
-}
+        relocate("com.infernalsuite.asp.loaders", "dev.danielmillar.asp.loaders")
+        relocate("org.jetbrains.kotlin", "dev.danielmillar.kotlin")
+        relocate("org.jetbrains.kotlinx", "dev.danielmillar.kotlinx")
+        relocate("org.spongepowered.configurate", "dev.danielmillar.configurate")
+    }
 
-tasks.processResources {
-    val props = mapOf("version" to version)
-    inputs.properties(props)
-    filteringCharset = "UTF-8"
-    filesMatching("plugin.yml") {
-        expand(props)
+    processResources {
+        val props = mapOf("version" to version)
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("paper-plugin.yml") {
+            expand(props)
+        }
+    }
+
+    assemble {
+        dependsOn(shadowJar)
     }
 }
